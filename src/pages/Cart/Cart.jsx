7860659b34ba9bba403dffe135b045e1f94ced7a
Saphaa mohamed <saphaasaphaamohamed@gmail.com>
2025-05-17 10:45:39 +1400

    fc

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,5 +1,5 @@
 import  { useContext } from 'react';
-import './Cart.css';
+import './cart.css';
 import { StoreContext } from '../../context/StoreContext';
 import{ useNavigate } from 'react-router-dom';
 
